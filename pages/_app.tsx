56f68316c3a28686a9ae48b66b7188d0b821f5be
y3owk1n <wongyeowkin@gmail.com>
2022-04-28 00:44:16 +0800

    updated dev & prod domain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,18 @@ import convexConfig from "../convex.json";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import { AuthProvider } from "@/lib/auth";
+import { devDomain, prodDomain } from "@/utils/constants";
 
 const convex = new ConvexReactClient(convexConfig.origin);
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const isDev = process.env.NODE_ENV === "development";
+
   return (
     <Auth0Provider
       // domain and clientId come from your Auth0 app dashboard
       domain="dev-7erd7tmk.jp.auth0.com"
       clientId="9hIBBfrcOpHEYGXvJw7g2DtaDCP8MDm7"
-      redirectUri={"http://localhost:3000"}
+      redirectUri={isDev ? devDomain : prodDomain}
       // allows auth0 to cache the authentication state locally
       cacheLocation="localstorage"
     >
---
+++ utils/constants.ts
@@ -0,0 +1,2 @@
+export const devDomain = "http://localhost:3000";
+export const prodDomain = "https://chat-for-fun.vercel.app";
